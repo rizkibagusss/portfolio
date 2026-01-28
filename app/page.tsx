eec236605d5a71f167bcd8e25df78f86af514c9f
rizkibagusss <rizkibagusariyanto@gmail.com>
2026-01-29 01:16:41 +0700

    init: setup Next.js portoflio project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Rizki Bagus</h1>
+      <p>Software Engineer</p>
+
+      <p>I told you bro, i can get remote job for my second job</p>
+    </main>
+  );
+}
