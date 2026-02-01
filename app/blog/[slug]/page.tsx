2941951a16420da69410f816570c313e37f0e889
rizkibagusss <rizkibagusariyanto@gmail.com>
2026-02-01 15:07:57 +0700

    feat: initial blog with Supabase integration

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -7,20 +7,24 @@ type Props = {
 export default async function BlogDetailPage({ params }: Props) {
   const { slug } = await params;
 
-  const { data: blog, error } = await supabase
+  const { data: blog } = await supabase
     .from("blogs")
     .select("*")
     .eq("slug", slug)
     .single();
 
-  if (error || !blog) {
+  if (!blog) {
     return <p>Blog tidak ditemukan</p>;
   }
 
   return (
-    <article>
-      <h1>{blog.title}</h1>
-      <p>{blog.content}</p>
+    <article className="max-w-3xl mx-auto py-12 px-4">
+      <h1 className="text-4xl font-bold mb-6">{blog.title}</h1>
+      <p className="text-sm text-gray-500 mb-10">
+        {new Date(blog.created_at).toLocaleDateString()}
+      </p>
+      <div className="prose max-w-none">{blog.content}</div>{" "}
+      <div className=""></div>
     </article>
   );
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,24 +4,35 @@ import { supabase } from "../lib/supabase";
 export default async function BlogPage() {
   const { data: blogs, error } = await supabase
     .from("blogs")
-    .select("id, title, slug")
+    .select("id, title, slug, created_at")
     .order("created_at", { ascending: false });
 
   if (error) {
-    return <p>Error loading blogs</p>;
+    return <p>Error mengambil data blog</p>;
   }
 
   return (
-    <main>
-      <h1>Blog</h1>
+    <main className="max-w-4xl mx-auto py-10 px-4">
+      <h1 className="text-3xl font-bold mb-8">Blog</h1>
 
-      <ul>
-        {blogs?.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.slug}`}>{blog.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className="grid gap-6">
+        <ul>
+          {blogs?.map((blog) => (
+            <li key={blog.id}>
+              <Link
+                href={`/blog/${blog.slug}`}
+                className="block border rounded-xl p-6 mb-3 hover:shadow-md transition"
+              >
+                <h2 className="text-xl font-semibold mb-2">{blog.title}</h2>
+
+                <p className="text-sm text-gray-500">
+                  {new Date(blog.created_at).toLocaleDateString()}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
